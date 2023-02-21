b3e2fc0db79f50e29f731b63b6478a3d2c84d0a0
Bootcamp <bootcamp@Abduls-MacBook-Air.local>
2023-02-21 08:13:12 -0700

    JS updates

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,9 +1,12 @@
 function searchCity(){
-    console.log("test")
-    var cityName= $("#form1").val()
-    console.log(cityName)
+    const nameOfCity = document.getElementById("cityInput");
 }
 
 
 $(".btn").on("click", searchCity)
 
+var weatherApi= "e0f2d656b846c3e3e712051ac66d9715"
+var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + weatherApi + "&units=" + "imperial"
+fetch(queryURL).then(res => {
+    return res.json()
+})
