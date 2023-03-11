ebcfa53bd31073dd741c2a07eff8f3cae3d5cf95
Bootcamp <bootcamp@Abduls-MacBook-Air.local>
2023-03-11 11:40:44 -0700

    deployed page bug

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -5,127 +5,123 @@ $(document).ready(function () {
     $("#search").click(function () {
         const city = $("#cityInput").val();
         storeCityName(city);
-        displaySearchHistory();
         var weatherApi = "e0f2d656b846c3e3e712051ac66d9715"
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherApi + "&units=imperial";
         fetch(queryURL).then(response => {
             return response.json();
         })
-        .then(data => {
-            
-            var date = new Date();
-            var cityName = data.name;
-            var currentDate = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
-            var weatherIcon = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
-            var weatherIconHTML = '<img src="'+ weatherIcon +'">';
-            $("#cityName").html( cityName + ' ('+ currentDate +')' + weatherIconHTML );
-            var temp = data.main.temp
-            document.getElementById("temp").textContent = "Temp: " + temp + " °F"
-            var wind = data.wind.speed
-            document.getElementById('wind').textContent = "Wind: " + wind + " mph"
-            var humidity = data.main.humidity
-            document.getElementById('humidity').textContent = "Humidity: " + humidity + " %"
-            var lat = (data.coord.lat);
-            var lon = (data.coord.lon);
-            var fiveDay = "https://api.openweathermap.org/data/2.5/forecast?" + "lat=" + lat + "&lon=" + lon + "&appid=" + weatherApi + "&units=imperial";
+            .then(data => {
 
-            return fetch(fiveDay).then(response => {
-                return response.json();
+                var date = new Date();
+                var cityName = data.name;
+                var currentDate = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
+                var weatherIcon = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+                var weatherIconHTML = '<img src="' + weatherIcon + '">';
+                $("#cityName").html(cityName + ' (' + currentDate + ')' + weatherIconHTML);
+                var temp = data.main.temp
+                document.getElementById("temp").textContent = "Temp: " + temp + " °F"
+                var wind = data.wind.speed
+                document.getElementById('wind').textContent = "Wind: " + wind + " mph"
+                var humidity = data.main.humidity
+                document.getElementById('humidity').textContent = "Humidity: " + humidity + " %"
+                var lat = (data.coord.lat);
+                var lon = (data.coord.lon);
+                var fiveDay = "https://api.openweathermap.org/data/2.5/forecast?" + "lat=" + lat + "&lon=" + lon + "&appid=" + weatherApi + "&units=imperial";
+
+                return fetch(fiveDay).then(response => {
+                    return response.json();
+                })
             })
-        })    
-        .then(data => {
-            // console.log(data);
+            .then(data => {
 
-            var baseDay = dayjs().format('D')
-            var startDay = Number(baseDay);
+                var dayOne = $("#date-1");
+                var iconOne = $("#emoji-1");
+                var iconOneSrc = "http://openweathermap.org/img/wn/" + data.list[3].weather[0].icon + ".png"
 
-            var dayOne = $("#date-1");
-            var iconOne = $("#emoji-1");
-            var iconOneSrc = "http://openweathermap.org/img/wn/" + data.list[3].weather[0].icon + ".png"
+                var tempOne = $("#temp-1");
+                var windOne = $("#wind-1");
+                var humidityOne = $("#humidity-1");
 
-            var tempOne = $("#temp-1");
-            var windOne = $("#wind-1");
-            var humidityOne = $("#humidity-1");
+                var dayOneBase = dayjs().format('M/DD/YYYY')
+                dayOne.text(dayOneBase);
+                iconOne.text("")
+                iconOne.append('<img src=' + iconOneSrc + '>')
+                tempOne.text(data.list[3].main.temp + "°F")
+                windOne.text(data.list[3].wind.speed + " mph")
+                humidityOne.text(data.list[3].main.humidity + "%")
 
-            var dayOneBase = startDay
-            dayOne.text(dayjs().format('M/' + dayOneBase + '/YYYY'));
-            iconOne.text("")
-            iconOne.append('<img src=' + iconOneSrc + '>')
-            tempOne.text(data.list[3].main.temp + "°F")
-            windOne.text(data.list[3].wind.speed + " mph")
-            humidityOne.text(data.list[3].main.humidity + "%")
+                var dayTwo = $("#date-2");
+                var iconTwo = $("#emoji-2");
+                var iconTwoSrc = "http://openweathermap.org/img/wn/" + data.list[11].weather[0].icon + ".png"
+                var tempTwo = $("#temp-2");
+                var windTwo = $("#wind-2");
+                var humidityTwo = $("#humidity-2");
 
-            var dayTwo = $("#date-2");
-            var iconTwo = $("#emoji-2");
-            var iconTwoSrc = "http://openweathermap.org/img/wn/" + data.list[11].weather[0].icon + ".png"
-            var tempTwo = $("#temp-2");
-            var windTwo = $("#wind-2");
-            var humidityTwo = $("#humidity-2");
+                var dayTwoBase = dayjs().add(1, "day").format('M/DD/YYYY')
+                dayTwo.text(dayTwoBase);
+                iconTwo.text("")
+                iconTwo.append('<img src=' + iconTwoSrc + '>')
+                tempTwo.text(data.list[11].main.temp + "°F")
+                windTwo.text(data.list[11].wind.speed + " mph")
+                humidityTwo.text(data.list[11].main.humidity + "%")
 
-            var dayTwoBase = dayOneBase + 1
-            dayTwo.text(dayjs().format('M/' + dayTwoBase + '/YYYY'));
-            iconTwo.text("")
-            iconTwo.append('<img src=' + iconTwoSrc + '>')
-            tempTwo.text(data.list[11].main.temp + "°F")
-            windTwo.text(data.list[11].wind.speed + " mph")
-            humidityTwo.text(data.list[11].main.humidity + "%")
+                var dayThree = $("#date-3");
+                var iconThree = $("#emoji-3");
+                var iconThreeSrc = "http://openweathermap.org/img/wn/" + data.list[19].weather[0].icon + ".png"
+                var tempThree = $("#temp-3");
+                var windThree = $("#wind-3");
+                var humidityThree = $("#humidity-3");
 
-            var dayThree = $("#date-3");
-            var iconThree = $("#emoji-3");
-            var iconThreeSrc = "http://openweathermap.org/img/wn/" + data.list[19].weather[0].icon + ".png"
-            var tempThree = $("#temp-3");
-            var windThree = $("#wind-3");
-            var humidityThree = $("#humidity-3");
+                var dayThreeBase = dayjs().add(2, "day").format('M/DD/YYYY')
+                dayThree.text(dayThreeBase);
+                iconThree.text("")
+                iconThree.append('<img src=' + iconThreeSrc + '>')
+                tempThree.text(data.list[19].main.temp + "°F")
+                windThree.text(data.list[19].wind.speed + " mph")
+                humidityThree.text(data.list[19].main.humidity + "%")
 
-            var dayThreeBase = dayTwoBase + 1
-            dayThree.text(dayjs().format('M/' + dayThreeBase + '/YYYY'));
-            iconThree.text("")
-            iconThree.append('<img src=' + iconThreeSrc + '>')
-            tempThree.text(data.list[19].main.temp + "°F")
-            windThree.text(data.list[19].wind.speed + " mph")
-            humidityThree.text(data.list[19].main.humidity + "%")
+                var dayFour = $("#date-4");
+                var iconFour = $("#emoji-4");
+                var iconFourSrc = "http://openweathermap.org/img/wn/" + data.list[27].weather[0].icon + ".png"
+                var tempFour = $("#temp-4");
+                var windFour = $("#wind-4");
+                var humidityFour = $("#humidity-4");
 
-            var dayFour = $("#date-4");
-            var iconFour = $("#emoji-4");
-            var iconFourSrc = "http://openweathermap.org/img/wn/" + data.list[27].weather[0].icon + ".png"
-            var tempFour = $("#temp-4");
-            var windFour = $("#wind-4");
-            var humidityFour = $("#humidity-4");
+                var dayFourBase = dayjs().add(3, "day").format('M/DD/YYYY')
+                dayFour.text(dayFourBase);
+                iconFour.text("")
+                iconFour.append('<img src=' + iconFourSrc + '>')
+                tempFour.text(data.list[27].main.temp + "°F")
+                windFour.text(data.list[27].wind.speed + " mph")
+                humidityFour.text(data.list[27].main.humidity + "%")
 
-            var dayFourBase = dayThreeBase + 1
-            dayFour.text(dayjs().format('M/' + dayFourBase + '/YYYY'));
-            iconFour.text("")
-            iconFour.append('<img src=' + iconFourSrc + '>')
-            tempFour.text(data.list[27].main.temp + "°F")
-            windFour.text(data.list[27].wind.speed + " mph")
-            humidityFour.text(data.list[27].main.humidity + "%")
+                var dayFive = $("#date-5");
+                var iconFive = $("#emoji-5");
+                var iconFiveSrc = "http://openweathermap.org/img/wn/" + data.list[35].weather[0].icon + ".png"
+                var tempFive = $("#temp-5");
+                var windFive = $("#wind-5");
+                var humidityFive = $("#humidity-5");
 
-            var dayFive = $("#date-5");
-            var iconFive = $("#emoji-5");
-            var iconFiveSrc = "http://openweathermap.org/img/wn/" + data.list[35].weather[0].icon + ".png"
-            var tempFive = $("#temp-5");
-            var windFive = $("#wind-5");
-            var humidityFive = $("#humidity-5");
-
-            var dayFiveBase = dayFourBase + 1
-            dayFive.text(dayjs().format('M/' + dayFiveBase + '/YYYY'));
-            iconFive.text("")
-            iconFive.append('<img src=' + iconFiveSrc + '>')
-            tempFive.text(data.list[35].main.temp + "°F")
-            windFive.text(data.list[35].wind.speed + " mph")
-            humidityFive.text(data.list[35].main.humidity + "%")
+                var dayFiveBase = dayjs().add(4, "day").format('M/DD/YYYY')
+                dayFive.text(dayFiveBase);
+                iconFive.text("")
+                iconFive.append('<img src=' + iconFiveSrc + '>')
+                tempFive.text(data.list[35].main.temp + "°F")
+                windFive.text(data.list[35].wind.speed + " mph")
+                humidityFive.text(data.list[35].main.humidity + "%")
 
 
-        })
+            })
+            .catch(
+                error => console.log('ERROR')
+            )
 
     })
-    .catch(
-        error => console.log('ERROR')
-    )
- 
+
+
 });
 
-function storeCityName ( cityName ) {
+function storeCityName(cityName) {
     if (cityName == "") return false;
     searchHistoryList.push(cityName);
     localStorage.setItem("searchHistoryList", JSON.stringify(searchHistoryList));
@@ -138,14 +134,16 @@ function displaySearchHistory() {
 
     for (i = 0; i < uniqueSearchHistory.length; i++) {
         var recentSearch = document.createElement('button');
-        recentSearch.classList.add('btn', 'btn-primary', 'recent-search');
+        recentSearch.classList.add('btn', 'btn-secondary', 'recent-search');
         recentSearch.setAttribute('data-city', uniqueSearchHistory[i]);
         recentSearch.textContent = uniqueSearchHistory[i];
         recentSearch.addEventListener("click", function () {
-           let cityName = $(this).data("city");
-           $("#cityInput").val(cityName);
-           $("#search").trigger("click");
+            let cityName = $(this).data("city");
+            $("#cityInput").val(cityName);
+            $("#search").trigger("click");
         });
-        $("#searchHistoryContainer").append( recentSearch );          
+        $("#searchHistoryContainer").append(recentSearch);
     }
+
 }
+displaySearchHistory();
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # 5-Day-Weather-Forecast
+A simple 5 Day Weather application that allows a user to save recently searched cities and to access information on wind speed, temperature, and humidity.
+
+## Deployed Application
+
+View at:  https://abasit96.github.io/5-Day-Weather-Forecast/
+
+
+## Requirements
+
+### User Story
+
+```
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+### Acceptance Criteria
+
+```
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+## Screenshots
+![Screenshot](https://raw.githubusercontent.com/abasit96/My-Work-Day-Scheduler/main/Assets/images/Scheduler.png)
